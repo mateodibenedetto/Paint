918d69876a904ab54a2d261c3a47144ed1644b84
Mateo <71787035+mateodibenedetto@users.noreply.github.com>
2022-03-17 21:53:47 -0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,10 +1,10 @@
 // "use strict";
 
 const canvas = document.getElementById('canvas');
-
 const dif = canvas.getBoundingClientRect(); // nos permite conocer la distancia entre el top y el techo y el left y el techo
-
 const context = canvas.getContext("2d");
+const clear = document.getElementById('clear');
+
 
 let painting,color,linewidth,difX,difY;
 
@@ -34,8 +34,12 @@ canvas.addEventListener("mouseup",()=> { // cuando se levanta
 
 const dibujar = (x1,y1,x2,y2) => {
     context.strokeStyle = color;
-    context.lineWidth = linewidth;
+    context.lineWidth = linewidth * 2;
     context.moveTo(x1,y1); // para mover el path a la posicion anterior
     context.lineTo(x2,y2); // crear la linea en la posicion
     context.stroke(); // crea la linea
 }
+
+clear.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+})
